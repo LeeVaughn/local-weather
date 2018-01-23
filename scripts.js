567d19ab656f4957ba422efaa63de11694c2983f
Lee Vaughn <32204461+LeeVaughn@users.noreply.github.com>
2018-01-22 21:18:56 -0800

    Update scripts.js
    Added if/else statement to change the background image and background colors based on the current weather. Added degree symbol to various temperature displays.

--- scripts.js
+++ scripts.js
@@ -22,6 +22,42 @@ function render(weatherData, celsius) {
 	$("#currentTemp").html(currentTemp);
 	$("#highTemp").html("High: " + highTemp);
 	$("#lowTemp").html("Low: " + lowTemp);
+
+	// Changes HTML background image and .main-header and .display background color based on current weather icon
+	if (currentIcon == "01d") {
+		$("html").css("background-image", "url(https://regularlyscheduledprogrammingblog.files.wordpress.com/2018/01/clear-sun.jpg)")
+		$(".main-header").css("background", "rgba(223, 89, 8, 0.75)")
+		$(".display").css("background", "rgba(223, 89, 8, 0.75)")
+	} else if (currentIcon == "01n") {
+		$("html").css("background-image", "url(https://regularlyscheduledprogrammingblog.files.wordpress.com/2018/01/clear-moon.jpg)")
+		$(".main-header").css("background", "rgba(8, 96, 156, 0.75)")
+		$(".display").css("background", "rgba(8, 96, 156, 0.75)")
+	} else if (currentIcon == "02d" || currentIcon == "03d" || currentIcon == "04d") {
+		$("html").css("background-image", "url(https://regularlyscheduledprogrammingblog.files.wordpress.com/2018/01/cloudy-day.jpg)")
+		$(".main-header").css("background", "rgba(41, 90, 231, 0.75)")
+		$(".display").css("background", "rgba(41, 90, 231, 0.75)")
+	} else if (currentIcon == "02n" || currentIcon == "03n" || currentIcon == "04n") {
+		$("html").css("background-image", "url(https://regularlyscheduledprogrammingblog.files.wordpress.com/2018/01/cloudy-night.jpg)")
+		$(".main-header").css("background", "rgba(2, 30, 115, 0.75)")
+		$(".display").css("background", "rgba(2, 30, 115, 0.75)")
+	} else if (currentIcon == "09d" || currentIcon == "10d" || currentIcon == "11d" || currentIcon == "09n" || currentIcon == "10n" || currentIcon == "11n") {
+		$("html").css("background-image", "url(https://regularlyscheduledprogrammingblog.files.wordpress.com/2018/01/rain.jpg)")
+		$(".main-header").css("background", "rgba(59, 64, 77, 0.75)")
+		$(".display").css("background", "rgba(59, 64, 77, 0.75)")
+	} else if (currentIcon == "13d" || currentIcon == "13n") {
+		$("html").css("background-image", "url(https://regularlyscheduledprogrammingblog.files.wordpress.com/2018/01/snow.jpg)")
+		$(".main-header").css("background", "rgba(105, 128, 195, 0.75)")
+		$(".display").css("background", "rgba(105, 128, 195, 0.75)")
+	} else if (currentIcon == "50d" || currentIcon == "50n") {
+		$("html").css("background-image", "url(https://regularlyscheduledprogrammingblog.files.wordpress.com/2018/01/mist.jpg)")
+		$(".main-header").css("background", "rgba(105, 128, 195, 0.75)")
+		$(".display").css("background", "rgba(105, 128, 195, 0.75)")
+	} else {
+		$("html").css("background-image", "url(https://regularlyscheduledprogrammingblog.files.wordpress.com/2018/01/global-weather.jpg)")
+		$(".main-header").css("background", "rgba(2, 30, 115, 0.75)")
+		$(".display").css("background", "rgba(2, 30, 115, 0.75)")
+	}
+
 }
 
 $(function () {
